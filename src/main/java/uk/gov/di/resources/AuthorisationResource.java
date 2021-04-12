9569e5f6aab8a04e14d5525ceb7b45fceb48f95e
dbes-gds <daniel.besbrode@digital.cabinet-office.gov.uk>
2021-04-12 16:49:57 +0100

    BAU: add logged-in check in authorize resource
    Co-authored-by: Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
    Co-authored-by: James Howes <james.howes@digital.cabinet-office.gov.uk>
    Co-authored-by: Chris Clayson <chris.clayson@@digital.cabinet-office.gov.uk>

--- src/main/java/uk/gov/di/resources/AuthorisationResource.java
+++ src/main/java/uk/gov/di/resources/AuthorisationResource.java
@@ -1,11 +1,18 @@
 package uk.gov.di.resources;
 
+import com.nimbusds.oauth2.sdk.AuthorizationCode;
+import com.nimbusds.oauth2.sdk.ParseException;
+import com.nimbusds.openid.connect.sdk.AuthenticationRequest;
+import com.nimbusds.openid.connect.sdk.AuthenticationResponse;
+import com.nimbusds.openid.connect.sdk.AuthenticationSuccessResponse;
+
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 import java.net.URI;
 
 @Path("/authorize")
@@ -13,10 +20,33 @@ public class AuthorisationResource {
 
     @GET
     @Produces(MediaType.TEXT_HTML)
-    public Response authorize() {
+    public Response authorize(@Context UriInfo uriInfo) throws ParseException {
+        boolean loggedIn = true;
+
+        var authenticationRequest = AuthenticationRequest.parse(uriInfo.getRequestUri());
+
+        if (loggedIn) {
+            AuthenticationResponse response = handleAuthenticationRequest(authenticationRequest);
+            return Response
+                    .status(302)
+                    .location(response.toSuccessResponse().toURI()).build();
+        } else {
             return Response
                     .status(302)
                     .location(URI.create("/login"))
                     .build();
+        }
+    }
+
+    public AuthenticationResponse handleAuthenticationRequest(AuthenticationRequest authenticationRequest) {
+        return new AuthenticationSuccessResponse(
+                authenticationRequest.getRedirectionURI(),
+                new AuthorizationCode(),
+                null,
+                null,
+                authenticationRequest.getState(),
+                null,
+                null
+        );
     }
 }
