056ecebcb90d9e30fa2cbf8fe4f9520f969f3ce0
Chris Clayson <chris.clayson@digital.cabinet-office.gov.uk>
2021-04-14 10:56:56 +0100

    OXSvsICt: Add tests for userinfo endpoint
    Co-authored-by: James Howes <james.howes@digital.cabinet-office.gov.uk>

--- src/main/java/uk/gov/di/resources/UserInfoResource.java
+++ src/main/java/uk/gov/di/resources/UserInfoResource.java
@@ -1,8 +1,11 @@
 package uk.gov.di.resources;
 
+import com.nimbusds.oauth2.sdk.ParseException;
 import com.nimbusds.oauth2.sdk.id.Subject;
+import com.nimbusds.oauth2.sdk.token.AccessToken;
 import com.nimbusds.openid.connect.sdk.claims.Gender;
 import com.nimbusds.openid.connect.sdk.claims.UserInfo;
+import org.apache.http.HttpStatus;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
@@ -17,12 +20,19 @@ public class UserInfoResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response userinfo(@HeaderParam("Authorization") String authorizationHeader) {
-        UserInfo userInfo = new UserInfo(new Subject());
-        userInfo.setFamilyName("Smith");
-        userInfo.setGivenName("John");
-        userInfo.setEmailAddress("john.smith@example.com");
-        userInfo.setGender(Gender.MALE);
 
-        return Response.status(200).entity(userInfo.toJSONObject()).build();
+        try {
+            AccessToken accessToken = AccessToken.parse(authorizationHeader);
+
+            UserInfo userInfo = new UserInfo(new Subject());
+            userInfo.setFamilyName("Smith");
+            userInfo.setGivenName("John");
+            userInfo.setEmailAddress("john.smith@example.com");
+            userInfo.setGender(Gender.MALE);
+
+            return Response.ok(userInfo.toJSONObject()).build();
+        } catch (ParseException e) {
+            return Response.status(HttpStatus.SC_UNAUTHORIZED).build();
+        }
     }
 }
