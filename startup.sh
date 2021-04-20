a976abc4119ed8d50b4a746fbaac8f6647e46333
Chris Clayson <chris.clayson@digital.cabinet-office.gov.uk>
2021-04-20 09:50:34 +0100

    BAU: Wait for Postgres and use Flyway for migrations
    Add a wait for the Postgres healthcheck to pass before continuing.
    Add Flyway in to docker-compose and run `flyway migrate` in `startup.sh` to ensure migrations are run.
    
    Co-authored-by: James Howes <james.howes@digital.cabinet-office.gov.uk>
    Co-authored-by: Daniel Besbrode <daniel.besbrode@digital.cabinet-office.gov.uk>

--- startup.sh
+++ startup.sh
@@ -1,11 +1,29 @@
 #!/usr/bin/env bash
 set -eu
 
-CONFIG_FILE=oidc-provider.yml
+function wait_for_docker_services() {
+  RUNNING=0
+  LOOP_COUNT=0
+  echo -n "Waiting for service(s) to become healthy ($*) ."
+  until [[ ${RUNNING} == $# || ${LOOP_COUNT} == 100 ]]; do
+    RUNNING=$(docker-compose ps -q "$@" | xargs docker inspect | jq -rc '[ .[] | select(.State.Health.Status == "healthy")] | length')
+    LOOP_COUNT=$((LOOP_COUNT + 1))
+    echo -n "."
+  done
+  if [[ ${LOOP_COUNT} == 100 ]]; then
+    echo "FAILED"
+    return 1
+  fi
+  echo " done!"
+  return 0
+}
 
 ./gradlew installDist
 
 docker-compose down || true
-docker-compose up -d
+docker-compose up -d op-db
 
-./build/install/di-auth-oidc-provider/bin/di-auth-oidc-provider server $CONFIG_FILE
+wait_for_docker_services op-db
+docker-compose run flyway
+
+./gradlew run
