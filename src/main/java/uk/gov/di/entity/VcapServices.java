8481835c4287d3ad787242bda86bf2f22dd5a469
Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
2021-04-20 12:21:03 +0100

    ikXzZaOa: Add logging during VcapServices parsing

--- src/main/java/uk/gov/di/entity/VcapServices.java
+++ src/main/java/uk/gov/di/entity/VcapServices.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Map;
@@ -12,6 +14,8 @@ import java.util.Optional;
 import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_MISSING_CREATOR_PROPERTIES;
 
 public class VcapServices {
+
+    private static final Logger LOG = LoggerFactory.getLogger(VcapServices.class);
 
     @JsonIgnoreProperties(ignoreUnknown = true)
     public record Service(Credentials credentials) {}
@@ -29,12 +33,14 @@ public class VcapServices {
                 );
 
             if (services == null || !services.containsKey("postgres")) {
+                LOG.info("Unable to find 'postgres' key in VCAP_SERVICES");
                 return Optional.empty();
             }
 
             return services.get("postgres").stream().findFirst().map(Service::credentials);
 
         } catch (JsonProcessingException e) {
+            LOG.info("Unable to parse VCAP_SERVICES json");
             return Optional.empty();
         }
     }
