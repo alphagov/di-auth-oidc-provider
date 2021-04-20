d5b825ca5db07540e9c6cd7ef3bdaba736b7393a
Alan Carter <alan.carter@digital.cabinet-office.gov.uk>
2021-04-20 17:55:10 +0100

    4QAoG2dk: Start registration from login page
    * Added a second button to login page which begins registration journey with the given email address.
    * Removed redundant registration start page.
    
    Co-authored-by: Alex Wilson <alex.wilson@digital.cabinet-office.gov.uk>
    Co-authored-by: Chris Clayson <chris.clayson@digital.cabinet-office.gov.uk>

--- src/main/java/uk/gov/di/resources/RegistrationResource.java
+++ src/main/java/uk/gov/di/resources/RegistrationResource.java
@@ -1,30 +1,18 @@
 package uk.gov.di.resources;
 
 import io.dropwizard.views.View;
-import uk.gov.di.views.RegisterView;
 import uk.gov.di.views.SetPasswordView;
-import uk.gov.di.views.SuccessfulLoginView;
 import uk.gov.di.views.SuccessfulRegistration;
 
 import javax.validation.constraints.NotNull;
 import javax.ws.rs.FormParam;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriBuilder;
-import java.net.URI;
 
 @Path("/registration")
 public class RegistrationResource {
-
-    @GET
-    @Produces(MediaType.TEXT_HTML)
-    public View register() {
-        return new RegisterView();
-    }
 
     @POST
     @Produces(MediaType.TEXT_HTML)
--- src/main/java/uk/gov/di/views/RegisterView.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.di.views;
-
-import io.dropwizard.views.View;
-
-public class RegisterView extends View {
-    public RegisterView() {
-        super("register-emailaddress.mustache");
-    }
-}
