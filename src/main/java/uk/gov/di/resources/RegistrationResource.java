fb80901c416461dbbdae2971459a528969e6bc44
Chris Clayson <chris.clayson@digital.cabinet-office.gov.uk>
2021-04-21 14:16:00 +0100

    4QAoG2dk: Update successful registration page to match prototype
    Bring successful registration page content in line with provided prototype
    Call to action on registration successful page should take user to login successful ("You will now be passed back to the service") page.

--- src/main/java/uk/gov/di/resources/RegistrationResource.java
+++ src/main/java/uk/gov/di/resources/RegistrationResource.java
@@ -3,15 +3,19 @@ package uk.gov.di.resources;
 import io.dropwizard.views.View;
 import org.apache.http.HttpStatus;
 import uk.gov.di.views.SetPasswordView;
+import uk.gov.di.views.SuccessfulLoginView;
 import uk.gov.di.views.SuccessfulRegistration;
 
 import javax.validation.constraints.NotNull;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import java.net.URI;
 
 @Path("/registration")
 public class RegistrationResource {
@@ -35,4 +39,14 @@ public class RegistrationResource {
             return Response.status(HttpStatus.SC_BAD_REQUEST).entity(new SetPasswordView(email, authRequest, true)).build();
         }
     }
+
+    @POST
+    @Path("/continue")
+    @Produces(MediaType.TEXT_HTML)
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public Response validateLogin(
+            @FormParam("authRequest") String authRequest) {
+        return Response.ok(new SuccessfulLoginView(authRequest)).build();
+    }
+
 }
