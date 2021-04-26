229fbb6d70ca6d7f786f48c903591a6ee4722d7a
Chris Clayson <chris.clayson@digital.cabinet-office.gov.uk>
2021-04-26 17:26:30 +0100

    WIP:- Get the (M1) challenge response working for Cognito

--- src/main/java/uk/gov/di/services/CognitoSrpService.java
+++ src/main/java/uk/gov/di/services/CognitoSrpService.java
@@ -1,10 +1,13 @@
 package uk.gov.di.services;
 
 import com.nimbusds.oauth2.sdk.util.StringUtils;
+import com.nimbusds.srp6.BigIntegerUtils;
 import com.nimbusds.srp6.SRP6ClientCredentials;
+import com.nimbusds.srp6.SRP6ClientEvidenceContext;
 import com.nimbusds.srp6.SRP6ClientSession;
 import com.nimbusds.srp6.SRP6CryptoParams;
 import com.nimbusds.srp6.SRP6Routines;
+import com.nimbusds.srp6.URoutineContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient;
@@ -14,11 +17,14 @@ import software.amazon.awssdk.services.cognitoidentityprovider.model.ChallengeNa
 import software.amazon.awssdk.services.cognitoidentityprovider.model.InitiateAuthRequest;
 import software.amazon.awssdk.services.cognitoidentityprovider.model.InitiateAuthResponse;
 import software.amazon.awssdk.services.cognitoidentityprovider.model.RespondToAuthChallengeRequest;
+import software.amazon.awssdk.services.cognitoidentityprovider.model.RespondToAuthChallengeResponse;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 import java.math.BigInteger;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
 import java.security.SecureRandom;
 import java.util.Date;
 import java.util.HashMap;
@@ -44,36 +50,6 @@ public class CognitoSrpService extends CognitoService {
     }
 
     public String performSRPAuthentication(String username, String password) {
-        String authresult = null;
-
-        InitiateAuthRequest initiateAuthRequest = initiateUserSrpAuthRequest(username, password);
-
-        try {
-            InitiateAuthResponse initiateAuthResponse = cognitoClient.initiateAuth(initiateAuthRequest);
-            LOG.info("performSRPAuthentication:initiateAuthResponse: {}", initiateAuthResponse.toString());
-
-        } catch (final Exception ex) {
-            System.out.println("Exception" + ex);
-
-        }
-
-//            if (ChallengeNameType.PASSWORD_VERIFIER.toString().equals(initiateAuthResponse.challengeName())) {
-//                RespondToAuthChallengeRequest challengeRequest = userSrpAuthRequest(initiateAuthResult, password,
-//                        initiateAuthRequest.authParameters().get("SECRET_HASH"));
-//                RespondToAuthChallengeResult result = cognitoIdentityProvider.respondToAuthChallenge(challengeRequest);
-//                //System.out.println(result);
-//                System.out.println(CognitoJWTParser.getPayload(result.getAuthenticationResult().getIdToken()));
-//                authresult = result.getAuthenticationResult().getIdToken();
-//            }
-//        } catch (final Exception ex) {
-//            System.out.println("Exception" + ex);
-//
-//        }
-        return authresult;
-    }
-
-    private InitiateAuthRequest initiateUserSrpAuthRequest(String username, String password) {
-
         SRP6CryptoParams config = SRP6CryptoParams.getInstance();
         SRP6Routines srp6Routines = new SRP6Routines();
         // Generate client private and public values
@@ -81,7 +57,68 @@ public class CognitoSrpService extends CognitoService {
         config.getMessageDigestInstance().reset();
 
         var A = srp6Routines.computePublicClientValue(config.N, config.g, a);
-        Map<String, String> authParams = new HashMap<>();
+
+        String authresult = null;
+
+        InitiateAuthRequest initiateAuthRequest = initiateUserSrpAuthRequest(username, A);
+
+        try {
+            InitiateAuthResponse initiateAuthResponse = cognitoClient.initiateAuth(initiateAuthRequest);
+            LOG.info("performSRPAuthentication:initiateAuthResponse: {}", initiateAuthResponse.toString());
+
+            if (ChallengeNameType.PASSWORD_VERIFIER.toString().equals(initiateAuthResponse.challengeName())) {
+
+                RespondToAuthChallengeRequest challengeRequest = userSrpAuthRequest(config, initiateAuthResponse, password);
+                RespondToAuthChallengeResponse result = cognitoClient.respondToAuthChallenge(challengeRequest);
+
+                LOG.info("respondToAuthChallengeResult: {}", result.toString());
+
+                authresult = result.authenticationResult().idToken();
+            }
+        } catch (final Exception ex) {
+            System.out.println("Exception" + ex);
+
+        }
+        return authresult;
+    }
+
+    private RespondToAuthChallengeRequest userSrpAuthRequest(SRP6CryptoParams config,
+                                                             InitiateAuthResponse initiateAuthResponse,
+                                                             String password,
+                                                             BigInteger a,
+                                                             BigInteger A) {
+        var B = new BigInteger(initiateAuthResponse.challengeParameters().get("SRP_B"), 16);
+        var salt = new BigInteger(initiateAuthResponse.challengeParameters().get("SALT"), 16);
+        SRP6Routines srp6Routines = new SRP6Routines();
+        MessageDigest digest = config.getMessageDigestInstance();
+
+        var x = srp6Routines.computeX(digest ,
+                BigIntegerUtils.bigIntegerToBytes(salt),
+                password.getBytes(Charset.forName("UTF-8")));
+        digest.reset();
+
+        // Compute the session key
+        var k = srp6Routines.computeK(digest, config.N, config.g);
+		digest.reset();
+
+        var u = srp6Routines.computeU(digest, config.N, A, B);
+        digest.reset();
+
+        var S = srp6Routines.computeSessionKey(config.N, config.g, k, x, u, a, B);
+
+        var M1 = srp6Routines.computeClientEvidence(digest, A, B, S);
+        digest.reset();
+
+        Map<String, String> challengeResponses = new HashMap<>();
+        challengeResponses.put("", "");
+        return RespondToAuthChallengeRequest.builder()
+                .clientId(this.clientId)
+                .challengeName(ChallengeNameType.PASSWORD_VERIFIER)
+                .challengeResponses(challengeResponses)
+                .build();
+    }
+    private InitiateAuthRequest initiateUserSrpAuthRequest(String username, BigInteger A) {
+        Map<String, String> authParams = new HashMap<>();ChallengeNameType.PASSWORD_VERIFIER
         authParams.put("USERNAME", username);
         authParams.put("SRP_A", A.toString(16));
 
