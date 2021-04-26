d2c423393da40cd786c7dd1b6c31855b8d858b9b
Chris Clayson <chris.clayson@digital.cabinet-office.gov.uk>
2021-04-26 14:48:14 +0100

    iffpebxE: Add step 3 of authentication
    Add the final step of the SRP negotiation and return true or false from `login` method.
    Refactoring of SRPUserService to ensure new SRP6ServerSession object for each login attempt.
    
    Co-authored-by: James Howes <james.howes@digital.cabinet-office.gov.uk>
    Co-authored-by: Daniel Besbrode <daniel.besbrode@digital.cabinet-office.gov.uk>

--- src/main/java/uk/gov/di/services/SRPUserService.java
+++ src/main/java/uk/gov/di/services/SRPUserService.java
@@ -16,13 +16,8 @@ import java.util.Map;
 
 public class SRPUserService implements AuthenticationService{
 
-    private SRP6ServerSession srp6ServerSession;
     private Map<String, SRP6Credentials> credentialsMap = new HashMap<>();
-
-    public SRPUserService() {
-        SRP6CryptoParams config = SRP6CryptoParams.getInstance();
-        this.srp6ServerSession = new SRP6ServerSession(config);
-    }
+    private SRP6CryptoParams config = SRP6CryptoParams.getInstance();
 
     @Override
     public boolean userExists(String email) {
@@ -32,8 +27,10 @@ public class SRPUserService implements AuthenticationService{
     @Override
     public boolean signUp(String email, String password) {
         SRP6CryptoParams config = SRP6CryptoParams.getInstance();
+
         SRP6VerifierGenerator gen = new SRP6VerifierGenerator(config);
-        BigInteger salt = new BigInteger(gen.generateRandomSalt());
+        BigInteger salt = new BigInteger(BigInteger.ONE.signum(), gen.generateRandomSalt());
+
         BigInteger verifier = gen.generateVerifier(salt, email, password);
         credentialsMap.put(email, new SRP6Credentials(salt.toString(16), verifier.toString(16)));
         return true;
@@ -46,8 +43,20 @@ public class SRPUserService implements AuthenticationService{
 
     @Override
     public boolean login(String email, String password) {
-        SRPStep1Response srpStep1Response = step1(email);
-        var M2 = step2(srpStep1Response);
+        if (userExists(email)) {
+            SRP6ClientSession srp6ClientSession = new SRP6ClientSession();
+            SRP6ServerSession srp6ServerSession = new SRP6ServerSession(config);
+
+            try {
+                srp6ClientSession.step1(email, password);
+                SRPStep1Response srpStep1Response = serverStep1(srp6ServerSession, email);
+                SRP6ClientCredentials credentials = clientStep2(srp6ClientSession, srpStep1Response);
+                BigInteger M2 = serverStep2(srp6ServerSession, credentials.A, credentials.M1);
+                return clientStep3(srp6ClientSession, M2);
+            } catch (SRP6Exception e) {
+
+            }
+        }
         return false;
     }
 
@@ -61,7 +70,7 @@ public class SRPUserService implements AuthenticationService{
         return null;
     }
 
-    private SRPStep1Response step1(String email) {
+    private SRPStep1Response serverStep1(SRP6ServerSession srp6ServerSession, String email) {
         var credentials = credentialsMap.get(email);
         BigInteger salt = new BigInteger(credentials.salt(), 16);
         BigInteger verifier = new BigInteger(credentials.verifier(), 16);
@@ -70,29 +79,25 @@ public class SRPUserService implements AuthenticationService{
         return new SRPStep1Response(credentials.salt(), b.toString(16));
     }
 
-    private BigInteger step2(SRPStep1Response srpStep1Response) {
+    private SRP6ClientCredentials clientStep2(SRP6ClientSession srp6ClientSession, SRPStep1Response srpStep1Response) throws SRP6Exception {
 
         SRP6CryptoParams config = SRP6CryptoParams.getInstance();
         SRP6ClientCredentials cred = null;
 
-        SRP6ClientSession srp6ClientSession = new SRP6ClientSession();
-
-        try {
-            cred = srp6ClientSession.step2(config, new BigInteger(srpStep1Response.salt(), 16), new BigInteger(srpStep1Response.B(),16));
-
-        } catch (SRP6Exception e) {
-            // Invalid server 'B'
-        }
-
-       return serverStep2(cred.A, cred.M1);
+        return srp6ClientSession.step2(config, new BigInteger(srpStep1Response.salt(), 16), new BigInteger(srpStep1Response.B(),16));
     }
 
-    private BigInteger serverStep2(final BigInteger A, final BigInteger M1) {
+    private BigInteger serverStep2(SRP6ServerSession srp6ServerSession, final BigInteger A, final BigInteger M1) throws SRP6Exception {
+        return srp6ServerSession.step2(A, M1);
+    }
+
+    private boolean clientStep3(SRP6ClientSession srp6ClientSession, final BigInteger M2) {
         try {
-            return srp6ServerSession.step2(A, M1);
+            srp6ClientSession.step3(M2);
+            return true;
         } catch (SRP6Exception e) {
-            // User authentication failed
-            throw new RuntimeException(e);
+            e.printStackTrace();
+            return false;
         }
     }
 
