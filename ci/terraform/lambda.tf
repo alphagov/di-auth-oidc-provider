19a93875df39ddfe20956282fe2aeefe42003d5f
dbes-gds <daniel.besbrode@digital.cabinet-office.gov.uk>
2021-05-19 11:50:47 +0100

    LTG-94: add api gateway terrform to call lambda

--- ci/terraform/lambda.tf
+++ ci/terraform/lambda.tf
@@ -28,9 +28,15 @@ resource "aws_lambda_function" "test_lambda" {
 
   runtime = "java11"
 
-  environment {
-    variables = {
-      foo = "bar"
-    }
-  }
 }
+
+//resource "aws_lambda_permission" "apigw" {
+//  statement_id  = "AllowAPIGatewayInvoke"
+//  action        = "lambda:InvokeFunction"
+//  function_name = aws_lambda_function.test_lambda.function_name
+//  principal     = "apigateway.amazonaws.com"
+//
+//  # The "/*/*" portion grants access from any method on any resource
+//  # within the API Gateway REST API.
+//  source_arn = "${aws_api_gateway_rest_api.di-oidc-api.execution_arn}/*/*"
+//}
